764cb60ff4351ced99aa6c433ef6f473e9231713
Frank Henard <frank@mirusresearch.com>
2018-02-14 11:35:36 -0600

    initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# django\_es\_indexer
+
+## to deploy
+
+    python setup.py sdist upload -r pypi
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django_es_indexer",
+    version="2.1.4",
+    description="Elasticsearch indexing from a django app",
+    author="Mirus Research",
+    author_email="frank@mirusresearch.com",
+    packages=find_packages(),
+    url='https://bitbucket.org/mirusresearch/django_es_indexer',
+    license='MIT license, see LICENSE.txt',
+    install_requires=[
+        'six>=1.11.0',
+    ],
+)
