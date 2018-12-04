bf31dca3cb194d12a739f59edc23bceccc1519ab
Frank Henard <frank@mirusresearch.com>
2018-12-03 19:37:47 -0600

    update url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Mirus Research",
     author_email="frank@mirusresearch.com",
     packages=find_packages(),
-    url='https://bitbucket.org/mirusresearch/django_es_indexer',
+    url='https://github.com/mirusresearch/django_es_indexer',
     license='MIT license, see LICENSE.txt',
     install_requires=[
         'six>=1.11.0',
